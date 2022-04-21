9a49f9df810f126ee3a56479d2493a022960a708
rbenne30 <ryannebennett1@outlook.com>
2022-04-21 16:49:33 -0400

    update html

--- src/generate-page.js
+++ src/generate-page.js
@@ -45,11 +45,12 @@ const generateTeam = (team) => {
     console.log(html);
     return `<!DOCTYPE html>
        <html lang="en">
+    <link rel="stylesheet" type="text/css" href="./style.css">
        <head>
            <meta charset="UTF-8">
            <meta http-equiv="X-UA-Compatible" content="IE=edge">
            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-           <title>Document</title>
+           <title>TeamProfile Generator</title>
        </head>
        <body>
        ${html} 
